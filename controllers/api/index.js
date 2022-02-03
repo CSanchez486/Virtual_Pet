584be4ca962f2f26e083112cfa59b34aa7df1d99
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-03 13:44:04 -0500

    added entry point that connects index.js to homeRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
 
+const homeRoutes = require('./homeRoutes');
+
+// router uses homeRoutes as entry page
+router.use('/', homeRoutes);
+
 module.exports = router;
