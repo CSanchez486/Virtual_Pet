3180c488b993e563cc33c0259d78723fad817857
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-03 13:40:55 -0500

    there are two logins, added appropriate route for each portal

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,16 +1,23 @@
-const router = require('express').Router();
+`const router = require('express').Router();`
 const withAuth = require('../../utils/auth');
 const { User } = require('../../models');
 const { Vet } = require('../../models');
 
-// log-in route: Redirects page from login to main after log-in is completed
-router.get('/login', (req, res) => {
+// log-in route: Redirects page from login to main after log-in is completed for vet portal
+router.get('/login/vet', (req, res) => {
     if (req.session.logged_in) {
         res. redirect('/');
         return;
     }
 })
 
+// log-in route: Redirects page from login to main after log-in is completed for user portal
+router.get('/login/user', (req, res) => {
+    if (req.session.logged_in) {
+        res. redirect('/');
+        return;
+    }
+})
 
 res.render('login');
 
