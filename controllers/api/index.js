ef742a5908f8272affbeadf6acf1d6554651bd0b
Charlie Money <cmoney5748@gmail.com>
2022-02-04 13:06:39 -0500

    changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./homeRoutes');
+// const homeRoutes = require('../homeRoutes');
 
-// router uses homeRoutes as entry page
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+// // router uses homeRoutes as entry page
+// router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
 
