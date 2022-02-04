dd9064c765f2f2bfa227ff9d7721c8cad43041cd
bennyykim <94768507+bennyykim@users.noreply.github.com>
2022-02-04 14:01:50 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-<<<<<<< HEAD
 const apiRoutes =require('../api');
 const homeRoutes = require('../homeRoutes');
 const userPortal = require('./userPortalRoutes');
@@ -12,14 +11,6 @@ router.use('/api', apiRoutes);
 router.use('/user_signup', userPortal);
 router.use('/user', userRoutes);
 router.use('/vet', vetRoutes);
-
-=======
-// const homeRoutes = require('../homeRoutes');
-
-// // router uses homeRoutes as entry page
-// router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
->>>>>>> 57fb3ec546c879170c5da0fab257d6494ba3e932
 
 module.exports = router;
 
