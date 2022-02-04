ddf600e5b61afcc110c4916d3c6cfff99edeadb5
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-02-04 10:44:56 -0500

    completed try/catch for vet login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
-const withAuth = require('../../utils/auth');
-const { User } = require('../../models');
-const { Vet } = require('../../models');
+const withAuth = require('../utils/auth');
+const { User } = require('../models');
+const { Vet } = require('../models');
 
 // log-in route: Redirects page from login to main after log-in is completed for vet portal
-router.get('/login/vet', withAuth, async (req, res) => {
-    try { const vetData = await Vet.findAll ({
-        attributes: { exclude: ['password']},
-        order: [['name'],]
-        }),
-        )},
-
+router.get("/login/vet", withAuth, async (req, res) => {
+    try {
+        const vetData = await Vet.findAll({
+            attributes: { exclude: ["password"]},
+        });
+        const vet = vetData.map((project) => project.get({plain: true}));
+        res.render("main", {
+            users,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
         const vet = vetData.map((project) => project.get({plain: true}));
 
         res.render('main', {
@@ -28,6 +35,13 @@ router.get('/login/user', withAuth,  (req, res) => {
     }
 })
 
+router.get('/login/user', withAuth,  (req, res) => {
+    if (req.session.logged_in) {
+        res. redirect('/');
+        return;
+    }
+})
+
 // if logged the request to the homepage
 router.get('/login/vet', (req, res) => {
     if (req.session.logged_in) {
@@ -36,12 +50,7 @@ router.get('/login/vet', (req, res) => {
     }
 })
 
-router.get('/login/user', withAuth,  (req, res) => {
-    if (req.session.logged_in) {
-        res. redirect('/');
-        return;
-    }
-})
+
 
 res.render('login');
 
