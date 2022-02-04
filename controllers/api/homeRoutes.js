f70e2938a79c429573434f5314c34467c9a05e91
Emily Reese <emilyreese.ga@gmail.com>
2022-02-04 10:24:27 -0500

    I have uncommented out chris's code so she can work on it.

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -3,19 +3,19 @@ const withAuth = require('../../utils/auth');
 const { User } = require('../../models');
 const { Vet } = require('../../models');
 
-// // log-in route: Redirects page from login to main after log-in is completed for vet portal
-// router.get('/login/vet', withAuth, async (req, res) => {
-//     try { 
-//         const vetData = await Vet.findAll({
-//             attributes: { exclude: ['password']}, 
-//         });
+// log-in route: Redirects page from login to main after log-in is completed for vet portal
+router.get('/login/vet', withAuth, async (req, res) => {
+    try { 
+        const vetData = await Vet.findAll({
+            attributes: { exclude: ['password']}, 
+        });
     
-//         const vet = vetData.map((project) => project.get({plain: true}));
+        const vet = vetData.map((project) => project.get({plain: true}));
 
-//         res.render('main', {
-//             users,
-//             logged_in: req.session.logged_in,
-//         });
+        res.render('main', {
+            users,
+            logged_in: req.session.logged_in,
+        });
 
 
            
@@ -43,6 +43,6 @@ router.get('/login/user', withAuth,  (req, res) => {
     }
 })
 
-// res.render('login');
+res.render('login');
 
 module.exports = router;
