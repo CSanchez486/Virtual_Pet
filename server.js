e474a2cfa273afe878788e3e8c22cff20532fb01
Emily Reese <emilyreese.ga@gmail.com>
2022-02-07 22:11:36 -0500

    centered forums.

--- server.js
+++ server.js
@@ -50,6 +50,3 @@ sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 })
 
-app.get("/:consult", (req, res) => {
-    res.render("consult", {consultID: req.param.room});
-});
